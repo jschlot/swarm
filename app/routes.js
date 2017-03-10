8406697220b21239197f754a703689e8f577f33f
Jack Schlotthauer <jack.schlotthauer@gmail.com>
2017-03-09 22:05:30 -0800

    namespaced

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './modules/core/App';
-import GameViewContainer from './modules/gameview/container';
+import Gameview from './modules/gameview/container';
 
 export default (
 	<Route path="/" component={App}>
-		<IndexRoute component={GameViewContainer} />
+		<IndexRoute component={Gameview} />
 	</Route>
 );
