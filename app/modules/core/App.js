0db557056b96ed6153770aa8baf6fd8b3cafd252
Jack Schlotthauer <jack.schlotthauer@gmail.com>
2017-03-15 19:09:57 -0700

    added root class

--- app/modules/core/App.js
+++ app/modules/core/App.js
@@ -3,7 +3,7 @@ import React, { PropTypes } from 'react';
 import { reset } from './components/reset.scss';
 
 const App = ({ children }) =>
-    <div>
+    <div className="swarm">
         { children }
     </div>;
 
