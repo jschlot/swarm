82148a1103e1297885b6ddc5ec9d1c96e443ffa1
Jack Schlotthauer <jack.schlotthauer@gmail.com>
2017-03-19 12:28:15 -0700

    tabbing code sucks

--- app/modules/gameview/components/chapter/layout.js
+++ app/modules/gameview/components/chapter/layout.js
@@ -7,51 +7,46 @@ import Panel from '../panel/hoc';
 
 const Chapter = (props) => {
     const {
-        currentChapterIdx,
-        heading,
-        body,
-        decision,
-        chapterEnding,
-        onChoice,
-        onNext
+			currentChapterIdx,
+			heading,
+			body,
+			decision,
+			chapterEnding,
+			onChoice,
+			onNext
     } = props;
 
     return (
-			<div className="chapter">
-					<Panel timer="600">
-						<h2 className="chapter__heading">
-								{ heading }
-						</h2>
-					</Panel>
-					<Panel timer="1200">
-							{ body ? (
-									<div className="chapter__body">
-											{body}
-									</div>
-							) : null }
-
-							{ decision.options ? (
-								<div className="chapter__choices">
-										<div className="chapter__question">{ decision.text }</div>
-										{ decision.options.map((obj, index) => (
-												<div key={index} className="chapter__pick" onClick={() => onChoice(decision, obj, currentChapterIdx)}>
-														{obj.text}
+		    <div className="chapter">
+				    <Panel timer="600">
+					      <h2 className="chapter__heading">
+						        {heading}
+					      </h2>
+				    </Panel>
+				    <Panel timer="1200">
+					      {body ? (<div className="chapter__body">{body}</div>) : null}
+								{decision.options ? (
+								    <div className="chapter__choices">
+										    <div className="chapter__question">{decision.text}</div>
+							          {decision.options.map((obj, index) => (
+								            <div key={index} className="chapter__pick" onClick={() => onChoice(decision, obj, currentChapterIdx)}>
+									              {obj.text}
+								            </div>
+							          ))}
+										</div>
+								) : (
+							      <div>
+										    <div className="chapter___result">
+												    {chapterEnding}
+											  </div>
+											  <div className="chapter__next" onClick={onNext}>
+												    go on...
 												</div>
-										)) }
-								</div>
-							) : (
-								<div>
-									<div className="chapter___result">
-											{ chapterEnding }
-									</div>
-									<div className="chapter__next" onClick={onNext}>
-											go on...
-									</div>
-								</div>
-							) }
-					</Panel>
+										</div>
+						    )}
+            </Panel>
 			</div>
-    );
+		);
 };
 
 Chapter.propTypes = {
