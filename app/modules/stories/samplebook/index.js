477b55fd27e5d91f91c9a11fac6026cf50e37961
Jack Schlotthauer <jack.schlotthauer@gmail.com>
2017-03-26 13:37:19 -0700

    remove comments

--- app/modules/stories/samplebook/index.js
+++ app/modules/stories/samplebook/index.js
@@ -14,12 +14,3 @@ export const samplebook = {
     },
     episodes: [WHOAMI, GOINGPLACES, FOUNDANDLIST, GOINGPLACES, FOUNDANDLIST]
 };
-
-/*
-Books are structured as an array of episodes
-    each episode has a linked list set of chapters
-        each chapter has a story text and at least one option
-            each option can set weighted alignment scores, and can choose which chapter is next
-        if there are no more "next" chapters, then we proceed to a score screen for the chapter
-    if there are no more episodes, then we end the book, and proceed to the book score screen
-*/
