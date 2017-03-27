58105e42bfb6bcf982cbb866896094e6ebc876d6
Jack Schlotthauer <jack.schlotthauer@gmail.com>
2017-03-26 19:04:52 -0700

    story cleanup getting ready for a new story

--- app/modules/stories/index.js
+++ app/modules/stories/index.js
@@ -1,15 +1,13 @@
 import { WHOAMI } from './samplebook/whoami';
-import { GOINGPLACES } from './samplebook/goingplaces';
-import { FOUNDANDLIST } from './samplebook/foundandlost';
 
 export const samplebook = {
     meta: {
-        title: 'Sample Book',
+        title: 'Sample Story',
         keywords: 'tutorial, sample',
         description: [
             'in which we ask you a series of strange questions, and we find out a lot about who you are',
             ' as a person.'
         ]
     },
-    episodes: [WHOAMI, GOINGPLACES, FOUNDANDLIST]
+    episodes: [WHOAMI]
 };
